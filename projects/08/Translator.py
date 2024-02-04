78188814331163c5c84d72c1cab1e09cc6fc3298
su2u4 <Joey23.su@gmail.com>
2024-02-04 23:13:26 +0800

    第八章基礎部分完成

--- projects/08/Translator.py
+++ projects/08/Translator.py
@@ -1,11 +1,10 @@
-def translator(path):
-    name = path.split("\\")[-1].split(".")[0]
+import os
+
+
+def translator(text):
     r = []
-    command = ["@256\nD=M\n@SP\nA=M\nM=D"]
-    err = False
-    error = []
-    f = open(path, "r")
-    text = f.readlines()
+    command = ["@256\nD=A\n@SP\nM=D"]
+    error = 0
     for i in text:
         if i == "" or i == "\n":
             r.append(i)
@@ -20,98 +19,143 @@ def translator(path):
     if [] in text:
         text.remove([])
     for i in range(len(text)):
-        if text[i][0] == "add":
-            command.append("@SP\nA=M-1\nD=M\nA=A-1\nD=D+M\n@SP\nM=M-1\nA=M-1\nM=D")
+        if text[i][0] == "name":
+            name = text[i][1]
+        elif text[i][0] == "add":
+            command.append(f"//{text[i]}\n"+"@SP\nA=M-1\nD=M\nA=A-1\nD=D+M\n@SP\nM=M-1\nA=M-1\nM=D")
         elif text[i][0] == "sub":
-            command.append("@SP\nA=M-1\nD=M\nA=A-1\nD=M-D\n@SP\nM=M-1\nA=M-1\nM=D")
+            command.append(f"//{text[i]}\n"+"@SP\nA=M-1\nD=M\nA=A-1\nD=M-D\n@SP\nM=M-1\nA=M-1\nM=D")
         elif text[i][0] == "neg":
-            command.append("@SP\nA=M-1\nD=M\nD=D-M\nM=D-M")
+            command.append(f"//{text[i]}\n"+"@SP\nA=M-1\nD=M\nD=D-M\nM=D-M")
         elif text[i][0] == "eq":
-            command.append(
+            command.append(f"//{text[i]}\n"+
                 f"@SP\nA=M-1\nD=M\nA=A-1\nD=D-M\n@TRUE{i}\nD;JEQ\n@SP\nM=M-1\nA=M-1\nM=0\n@END{i}\n0;JMP\n(TRUE{i})\n@SP\nM=M-1\nA=M-1\nM=-1\n(END{i})"
             )
         elif text[i][0] == "gt":
-            command.append(
+            command.append(f"//{text[i]}\n"+
                 f"@SP\nA=M-1\nD=M\nA=A-1\nD=D-M\n@TRUE{i}\nD;JLT\n@SP\nM=M-1\nA=M-1\nM=0\n@END{i}\n0;JMP\n(TRUE{i})\n@SP\nM=M-1\nA=M-1\nM=-1\n(END{i})"
             )
         elif text[i][0] == "lt":
-            command.append(
+            command.append(f"//{text[i]}\n"+
                 f"@SP\nA=M-1\nD=M\nA=A-1\nD=D-M\n@TRUE{i}\nD;JGT\n@SP\nM=M-1\nA=M-1\nM=0\n@END{i}\n0;JMP\n(TRUE{i})\n@SP\nM=M-1\nA=M-1\nM=-1\n(END{i})"
             )
         elif text[i][0] == "and":
-            command.append("@SP\nA=M-1\nD=M\nA=A-1\nD=D&M\n@SP\nM=M-1\nA=M-1\nM=D")
+            command.append(f"//{text[i]}\n"+"@SP\nA=M-1\nD=M\nA=A-1\nD=D&M\n@SP\nM=M-1\nA=M-1\nM=D")
         elif text[i][0] == "or":
-            command.append("@SP\nA=M-1\nD=M\nA=A-1\nD=D|M\n@SP\nM=M-1\nA=M-1\nM=D")
+            command.append(f"//{text[i]}\n"+"@SP\nA=M-1\nD=M\nA=A-1\nD=D|M\n@SP\nM=M-1\nA=M-1\nM=D")
         elif text[i][0] == "not":
-            command.append("@SP\nA=M-1\nM=!M")
+            command.append(f"//{text[i]}\n"+"@SP\nA=M-1\nM=!M")
         elif text[i][0] == "push":
             if text[i][1] == "argument":
-                command.append(f"@ARG\nD=M\n@{text[i][2]}\nA=D+A\nD=M\n@SP\nM=M+1\nA=M-1\nM=D")
+                command.append(f"//{text[i]}\n"+f"@ARG\nD=M\n@{text[i][2]}\nA=D+A\nD=M\n@SP\nM=M+1\nA=M-1\nM=D")
             elif text[i][1] == "local":
-                command.append(f"@LCL\nD=M\n@{text[i][2]}\nA=D+A\nD=M\n@SP\nM=M+1\nA=M-1\nM=D")
+                command.append(f"//{text[i]}\n"+f"@LCL\nD=M\n@{text[i][2]}\nA=D+A\nD=M\n@SP\nM=M+1\nA=M-1\nM=D")
             elif text[i][1] == "this":
-                command.append(f"@THIS\nD=M\n@{text[i][2]}\nA=D+A\nD=M\n@SP\nM=M+1\nA=M-1\nM=D")
+                command.append(f"//{text[i]}\n"+f"@THIS\nD=M\n@{text[i][2]}\nA=D+A\nD=M\n@SP\nM=M+1\nA=M-1\nM=D")
             elif text[i][1] == "that":
-                command.append(f"@THAt\nD=M\n@{text[i][2]}\nA=D+A\nD=M\n@SP\nM=M+1\nA=M-1\nM=D")
+                command.append(f"//{text[i]}\n"+f"@THAT\nD=M\n@{text[i][2]}\nA=D+A\nD=M\n@SP\nM=M+1\nA=M-1\nM=D")
             elif text[i][1] == "static":
-                command.append(f"@{name}.{text[i][2]}\nD=M\n@SP\nM=M+1\nA=M-1\nM=D")
+                command.append(f"//{text[i]}\n"+f"@{name}.{text[i][2]}\nD=M\n@SP\nM=M+1\nA=M-1\nM=D")
             elif text[i][1] == "pointer":
                 if text[i][2] == "0":
-                    command.append(f"@THIS\nD=M\n@SP\nM=M+1\nA=M-1\nM=D")
+                    command.append(f"//{text[i]}\n"+f"@THIS\nD=M\n@SP\nM=M+1\nA=M-1\nM=D")
                 elif text[i][2] == "1":
-                    command.append(f"@THAT\nD=M\n@SP\nM=M+1\nA=M-1\nM=D")
+                    command.append(f"//{text[i]}\n"+f"@THAT\nD=M\n@SP\nM=M+1\nA=M-1\nM=D")
             elif text[i][1] == "temp":
-                command.append(f"@R{5+int(text[i][2])}\nD=M\n@SP\nM=M+1\nA=M-1\nM=D")
+                command.append(f"//{text[i]}\n"+f"@R{5+int(text[i][2])}\nD=M\n@SP\nM=M+1\nA=M-1\nM=D")
             elif text[i][1] == "constant":
-                command.append(f"@{text[i][2]}\nD=A\n@SP\nM=M+1\nA=M-1\nM=D")
+                command.append(f"//{text[i]}\n"+f"@{text[i][2]}\nD=A\n@SP\nM=M+1\nA=M-1\nM=D")
             else:
-                err = True
-                error.append(f"line {i} {text[i]}")
+                error += 1
+                print("line", i, text[i], "\n------\n", command[-1], "\n------")
         elif text[i][0] == "pop":
             if text[i][1] == "argument":
-                command.append(f"@ARG\nD=M\n@{text[i][2]}\nD=D+A\n@R13\nM=D\n@SP\nM=M-1\nA=M\nD=M\n@R13\nA=M\nM=D")
+                command.append(f"//{text[i]}\n"+f"@ARG\nD=M\n@{text[i][2]}\nD=D+A\n@R13\nM=D\n@SP\nM=M-1\nA=M\nD=M\n@R13\nA=M\nM=D")
             elif text[i][1] == "local":
-                command.append(f"@LCL\nD=M\n@{text[i][2]}\nD=D+A\n@R13\nM=D\n@SP\nM=M-1\nA=M\nD=M\n@R13\nA=M\nM=D")
+                command.append(f"//{text[i]}\n"+f"@LCL\nD=M\n@{text[i][2]}\nD=D+A\n@R13\nM=D\n@SP\nM=M-1\nA=M\nD=M\n@R13\nA=M\nM=D")
             elif text[i][1] == "this":
-                command.append(f"@THIS\nD=M\n@{text[i][2]}\nD=D+A\n@R13\nM=D\n@SP\nM=M-1\nA=M\nD=M\n@R13\nA=M\nM=D")
+                command.append(f"//{text[i]}\n"+f"@THIS\nD=M\n@{text[i][2]}\nD=D+A\n@R13\nM=D\n@SP\nM=M-1\nA=M\nD=M\n@R13\nA=M\nM=D")
             elif text[i][1] == "that":
-                command.append(f"@THAt\nD=M\n@{text[i][2]}\nD=D+A\n@R13\nM=D\n@SP\nM=M-1\nA=M\nD=M\n@R13\nA=M\nM=D")
+                command.append(f"//{text[i]}\n"+f"@THAT\nD=M\n@{text[i][2]}\nD=D+A\n@R13\nM=D\n@SP\nM=M-1\nA=M\nD=M\n@R13\nA=M\nM=D")
             elif text[i][1] == "static":
-                command.append(f"@SP\nM=M-1\nA=M\nD=M\n@{name}.{text[i][2]}\nM=D")
+                command.append(f"//{text[i]}\n"+f"@SP\nM=M-1\nA=M\nD=M\n@{name}.{text[i][2]}\nM=D")
             elif text[i][1] == "pointer":
                 if text[i][2] == "0":
-                    command.append(f"@SP\nM=M-1\nA=M\nD=M\n@THIS\nM=D")
+                    command.append(f"//{text[i]}\n"+f"@SP\nM=M-1\nA=M\nD=M\n@THIS\nM=D")
                 elif text[i][2] == "1":
-                    command.append(f"@SP\nM=M-1\nA=M\nD=M\n@THAT\nM=D")
+                    command.append(f"//{text[i]}\n"+f"@SP\nM=M-1\nA=M\nD=M\n@THAT\nM=D")
             elif text[i][1] == "temp":
-                command.append(f"@SP\nM=M-1\nA=M\nD=M\n@R{5+int(text[i][2])}\nM=D")
+                command.append(f"//{text[i]}\n"+f"@SP\nM=M-1\nA=M\nD=M\n@R{5+int(text[i][2])}\nM=D")
             else:
-                err = True
-                error.append(f"line {i} {text[i]}")
+                error += 1
+                print("line", i, text[i], "\n------\n", command[-1], "\n------")
         elif text[i][0] == "label":
-            command.append(f"({name}${text[i][1]})")
+            command.append(f"//{text[i]}\n"+f"({name}${text[i][1]})")
         elif text[i][0] == "goto":
-            command.append(f"@{name}${text[i][1]}\n0;JMP")
+            command.append(f"//{text[i]}\n"+f"@{name}${text[i][1]}\n0;JMP")
         elif text[i][0] == "if-goto":
-            command.append(f"@SP\nM=M-1\nA=M\nD=M\n@{name}${text[i][1]}\nD;JNE")
+            command.append(f"//{text[i]}\n"+f"@SP\nM=M-1\nA=M\nD=M\n@{name}${text[i][1]}\nD;JNE")
+        elif text[i][0] == "call":
+            command.append(f"//{text[i]}\n"+
+                f"@return{i}\nD=M\n@SP\nM=M+1\nA=M-1\nM=D\n@LCL\nD=M\n@SP\nM=M+1\nA=M-1\nM=D\n@ARG\nD=M\n@SP\nM=M+1\nA=M-1\nM=D\n@THIS\nD=M\n@SP\nM=M+1\nA=M-1\nM=D\n@THAT\nD=M\n@SP\nM=M+1\nA=M-1\nM=D\n@SP\nD=M\n@{text[i][2]}\nD=D-A\n@5\nD=D-A\n@ARG\nM=D\n@SP\nD=M\n@LCL\nM=D\n@{text[i][1]}\n0;JMP\n(return{i})"
+            )
+        elif text[i][0] == "function":
+            command.append(f"//{text[i]}\n"+
+                f"({text[i][1]})\n(loop{i})\n@SP\nD=M\nA=D\nM=0\nD=D+1\n@SP\nM=D\n@{text[i][2]}\nD=A-D\n@loop{i}\nD;JGT\n@{text[i][2]}\nD=A\n@SP\nM=M-D"
+            )
+        elif text[i][0] == "return":
+            command.append(f"//{text[i]}\n"+
+                f"@LCL\nD=M\n@FRAME{i}\nM=D\n@5\nD=D-A\n@RET\nM=D\n@SP\nM=M-1\nA=M\nD=M\n@ARG\nM=D\n@SP\nM=D+1\n@FRAME{i}\nD=M\n@1\nD=D-A\n@THAT\nM=D\n@FRAME{i}\nD=M\n@2\nD=D-A\n@THIS\nM=D\n@FRAME{i}\nD=M\n@3\nD=D-A\n@ARG\nM=D\n@FRAME{i}\nD=M\n@4\nD=D-A\n@LCL\nM=D\n@RET\nA=M\n0;JMP"
+            )
         else:
-            err = True
-            error.append(f"line {i} {text[i]}")
-        # print(i, text[i], command[-1])
-    command.append("(END)\n@END\n0;JMP")
+            error += 1
+            print("line", i, text[i], "\n------\n", command[-1], "\n------")
     command = "\n".join(command)
-    f.close()
-    if err:
-        print(f"Error x{len(error)}")
-        for i in error:
-            print(i)
+    if error > 0:
+        print(f"Error x{error}")
+        exit()
     else:
-        path = path.split(".")
-        f = open(path[0] + ".asm", "w")
-        f.write(command)
-        f.close()
+        return command
+
+
+def file(path):
+    result = []
+    for f in os.listdir(path):
+        if os.path.isfile(os.path.join(path, f)):
+            result.append(os.path.join(path, f))
+        else:
+            result += file(os.path.join(path, f))
+    return result
 
 
 if __name__ == "__main__":
     path = input("file path and name:")
-    translator(path)
+    if ".vm" in path:
+        name = path.split("\\")[-1].split(".")[0]
+        text = [f"name {name}"]
+        f = open(path, "r")
+        text = f.readlines()
+        f.close()
+        command = translator(text)
+        path = path.split(".")
+        f = open(path[0] + ".asm", "w")
+        f.write(command)
+        f.close()
+    else:
+        if "C:\\Users\\joey2\\桌面\\nand2tetris\\" in path:
+            result = file(path)
+        else:
+            result = file("C:\\Users\\joey2\\桌面\\nand2tetris\\" + path)
+        text = []
+        for i in result:
+            if ".vm" in i:
+                name = i.split("\\")[-1].split(".")[0]
+                text.append(f"name {name}")
+                f = open(i, "r")
+                text += f.readlines()
+                f.close()
+        command = translator(text)
+        name = path.split("\\")[-1]
+        f = open(f"{path}\\{name}.asm", "w")
+        f.write(command)
+        f.close()
