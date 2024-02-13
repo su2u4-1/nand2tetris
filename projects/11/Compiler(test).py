2c5507563e413ad174af5b8cb751c3cda9a27b5c
su2u4 <Joey23.su@gmail.com>
2024-02-13 16:30:35 +0800

    temp

--- projects/11/Compiler(test).py
+++ projects/11/Compiler(test).py
@@ -36,8 +36,6 @@ l = [
     "<symbol> = </symbol>",
 ]
 temp = []
-symbolClass = {}
-symbol = []
 
 
 # 此函數會將嵌套的列表展開為一個平面列表
@@ -718,121 +716,6 @@ def compileExpressionList(text: list):
     return xml
 
 
-def compiler(text: list):
-    f0 = False
-    t0 = []
-    f1 = False
-    t1 = []
-    f2 = False
-    t2 = []
-    n0 = 0
-    n1 = 0
-    for i in range(len(text)):
-        text[i] = text[i].strip()
-    for i in text:
-        if i == "<class>":
-            s = -1
-            n0 = 0
-        if i == "<subroutineDec>":
-            s += 1
-            n1 = 0
-            if len(symbol) < s + 1:
-                symbol.append({})
-
-        if i == "<classVarDec>":
-            f0 = True
-        if f0:
-            t0.append(i)
-        if i == "</classVarDec>":
-            f0 = False
-            n0 = classVar(t0, n0)
-            t0 = []
-
-        if i == "<varDec>":
-            f1 = True
-        if f1:
-            t1.append(i)
-        if i == "</varDec>":
-            f1 = False
-            n1 = var(t1, s, n1)
-            t1 = []
-
-        if i == "<parameterList>":
-            f2 = True
-        if f2:
-            t2.append(i)
-        if i == "</parameterList>":
-            f2 = False
-            n1 = parameterList(t2, s, n1)
-            t2 = []
-    for i in range(len(text)):
-        if text[i] == "<keyword> class </keyword>":
-            className = " ".join(text[i + 1].split()[1:-1])
-        elif text[i] == "":
-            pass
-
-
-def var(text, s, n):
-    for i in range(len(text)):
-        if text[i] == "<keyword> var </keyword>":
-            type = " ".join(text[i + 1].split()[1:-1])
-            if text[i + 1].startswith("<identifier>") and text[i + 1].endswith("</identifier>"):
-                pass
-            if text[i + 2].startswith("<identifier>") and text[i + 2].endswith("</identifier>"):
-                name = " ".join(text[i + 2].split()[1:-1])
-                symbol[s][name] = [type, "local", n]
-                n += 1
-        elif text[i] == "<symbol> , </symbol>":
-            if text[i + 1].startswith("<identifier>") and text[i + 1].endswith("</identifier>"):
-                name = " ".join(text[i + 1].split()[1:-1])
-                symbol[s][name] = [type, "local", n]
-                n += 1
-        elif text[i] == "<symbol> ; </symbol>":
-            return n
-
-
-def classVar(text, n):
-    for i in range(len(text)):
-        if text[i] in ["<keyword> field </keyword>", "<keyword> static </keyword>"]:
-            cl = " ".join(text[i].split()[1:-1])
-            type = " ".join(text[i + 1].split()[1:-1])
-            if text[i + 1].startswith("<identifier>") and text[i + 1].endswith("</identifier>"):
-                pass
-            if text[i + 2].startswith("<identifier>") and text[i + 2].endswith("</identifier>"):
-                name = " ".join(text[i + 2].split()[1:-1])
-                symbolClass[name] = [type, cl, n]
-                n += 1
-        elif text[i] == "<symbol> , </symbol>":
-            if text[i + 1].startswith("<identifier>") and text[i + 1].endswith("</identifier>"):
-                name = " ".join(text[i + 1].split()[1:-1])
-                symbolClass[name] = [type, cl, n]
-                n += 1
-        elif text[i] == "<symbol> ; </symbol>":
-            return n
-
-
-def parameterList(text, s, n):
-    if len(text) > 2:
-        type = " ".join(text[0].split()[1:-1])
-        if text[0].startswith("<identifier>") and text[0].endswith("</identifier>"):
-            pass
-        if text[2].startswith("<identifier>") and text[2].endswith("</identifier>"):
-            name = " ".join(text[2].split()[1:-1])
-            symbol[s][name] = [type, "argument", n]
-            n += 1
-    if len(text) > 4:
-        for i in range(len(text)):
-            if text[i] == "<symbol> , </symbol>":
-                type = " ".join(text[i + 1].split()[1:-1])
-                if text[i + 1].startswith("<identifier>") and text[i + 1].endswith("</identifier>"):
-                    pass
-                if text[i + 2].startswith("<identifier>") and text[i + 2].endswith("</identifier>"):
-                    name = " ".join(text[i + 2].split()[1:-1])
-                    symbol[s][name] = [type, "argument", n]
-                    n += 1
-    return n
-
-
 def listAllFiles(path):
     result = []
     for f in os.listdir(path):
@@ -860,4 +743,3 @@ if __name__ == "__main__":
             f = open(i.split(".")[0] + "_M.xml", "w")
             f.write("\n".join(text))
             f.close()
-            text = compiler(text)
