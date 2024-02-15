4b4a8775ca0166e66ac27b041152d8e860539a39
su2u4 <Joey23.su@gmail.com>
2024-02-16 01:33:44 +0800

    parameterList and subroutineBody

--- projects/10/Parser.py
+++ projects/10/Parser.py
@@ -44,21 +44,6 @@ class xml:
         self.tag = tag
 
 
-# 移除列表裡的特定元素
-def removeE(l: list, e):
-    t = []
-    if type(e) == list:
-        e = set(e)
-        for i in l:
-            if i not in e:
-                t.append(i)
-    else:
-        for i in l:
-            if i != e:
-                t.append(i)
-    return t
-
-
 def grammarAnalyzer(text: list):
     for i in range(len(text)):
         text[i] = xml(text[i][0], text[i][1])
@@ -74,34 +59,62 @@ class Compiler:
         self.ia = 0
         self.f = "class"
 
+    def addCode(self, text: str):
+        self.CodeList.append("  " * self.ia + text)
+
     def Compile(self):
         for i in range(len(self.source)):
             if self.source[i].text == "<end> exit </end>":
                 break
-            if self.f == "class":
-                self.CompilerClass(self.source[i], self.source[i + 1])
-            elif self.f == "subroutineDec":
-                self.ComplierSubroutineDec(self.source[i], self.source[i + 1])
+            match self.f:
+                case "class":
+                    self.CompilerClass(self.source[i], self.source[i + 1])
+                case "subroutineDec":
+                    self.ComplierSubroutineDec(self.source[i], self.source[i + 1])
+                case "parameterList":
+                    self.CompilerParameterList(self.source[i], self.source[i + 1])
+                case "subroutineBody":
+                    self.CompilerSubroutineBody(self, self.source[i], self.source[i + 1])
 
     def CompilerClass(self, now: xml, next: xml):
         if now.tag == "keyword" and now.content == "class":
             self.CodeList.append("<class>")
             self.ia += 1
-            self.CodeList.append("  " * self.ia + now.text)
+            self.addCode(now.text)
         elif now.tag == "symbol" and now.content == "{":
-            self.CodeList.append("  " * self.ia + now.text)
-            self.CodeList.append("<subroutineDec>")
+            self.addCode(now.text)
+            self.addCode("<subroutineDec>")
             self.ia += 1
             self.f = "subroutineDec"
-        elif now.tag == "symbol" and now.content == "}":
-            self.ia -= 1
-            self.f = "class"
-            self.CodeList.append("  " * self.ia + now.text)
-            self.CodeList.append("</subroutineDec>")
         elif now.tag == "identifier":
-            self.CodeList.append("  " * self.ia + now.text)
+            self.addCode(now.text)
         elif next.tag == "end" and next.content == "exit":
             self.CodeList.append("</class>")
 
     def ComplierSubroutineDec(self, now: xml, next: xml):
+        if now.tag == "symbol":
+            if now.content == "(":
+                self.addCode(now.text)
+                self.addCode("<parameterList>")
+                self.ia += 1
+                self.f = "parameterList"
+            elif now.content == "{":
+                self.addCode("<subroutineBody>")
+                self.addCode(now.text)
+                self.ia += 1
+                self.f = "subroutineBody"
+            elif now.content == "}":
+                self.ia -= 1
+                self.f = "class"
+                self.addCode("</subroutineDec>")
+                self.addCode(now.text)
+        elif now.tag == "keyword" and now.content in ["function", "method", "constructor", "int", "char", "boolean", "void"]:
+            self.addCode(now.text)
+        elif now.tag == "identifier":
+            self.addCode(now.text)
+
+    def CompilerParameterList(self, now: xml, next: xml):
+        pass
+
+    def CompilerSubroutineBody(self, now: xml, next: xml):
         pass
